f7c436e300be7c5e901558c59a413f2857cdb064
lukaswoodtli <woodtli.lukas@gmail.com>
2021-04-22 10:54:52 +0200

    Fix template for fromPropertyTree

--- gen_code.sh
+++ gen_code.sh
@@ -12,8 +12,14 @@ generator_cmd=./openapi-generator-cli.sh
 $generator_cmd version
 
 # Extract templates from jar
-# $generator_cmd author template -g cpp-restbed-server
+# $generator_cmd author template -g cpp-restbed-server -o cpp_restbed_server_original_templates
 
-$generator_cmd generate -i petstore.yaml -g cpp-restbed-server -o restbed/generated_src -t cpp_restbed_server_custom_templates
+$generator_cmd generate \
+  -i petstore.yaml \
+  -g cpp-restbed-server \
+  -o restbed/generated_src \
+  -t cpp_restbed_server_custom_templates \
+  --global-property debugModels=true \
+  | tee out.txt
 
 #$generator_cmd generate -i petstore.yaml -g java -o java_client/generated_src

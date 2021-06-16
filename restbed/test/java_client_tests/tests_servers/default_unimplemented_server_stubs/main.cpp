1f76df7e826bc30bd18b7dacfdf3812faf06708d
lukaswoodtli <woodtli.lukas@gmail.com>
2021-06-16 14:38:37 +0200

    Add possibility to provide custom restbed settings and services to generated code

--- restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
+++ restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
@@ -3,6 +3,9 @@
 using namespace org::openapitools::server::api;
 
 int main() {
-  auto petApi = PetApi(1234);
+  const auto service = std::make_shared<restbed::Service>();
+  const auto settings = std::make_shared<restbed::Settings>();
+  settings->set_port(1234);
+  auto petApi = PetApi(service, settings);
   petApi.startService();
 }

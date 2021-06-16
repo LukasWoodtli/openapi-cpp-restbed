48bba9532a6c6488756950db8db9ea84686f8880
lukaswoodtli <woodtli.lukas@gmail.com>
2021-06-16 17:12:57 +0200

    Improve API of generated code

--- restbed/test/java_client_tests/java_client/src/test/java/test_default_unimplemented_server_stubs/PackageSettings.java
+++ restbed/test/java_client_tests/java_client/src/test/java/test_default_unimplemented_server_stubs/PackageSettings.java
@@ -1,5 +1,8 @@
 package test_default_unimplemented_server_stubs;
 
+import org.approvaltests.reporters.macosx.DiffMergeReporter;
+
 public class PackageSettings {
     public static String UseApprovalSubdirectory = "approval_files";
+    public static DiffMergeReporter UseReporter = DiffMergeReporter.INSTANCE;
 }
--- restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
+++ restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
@@ -1,4 +1,6 @@
 #include "api/PetApi.h"
+#include "api/StoreApi.h"
+#include "api/UserApi.h"
 
 using namespace org::openapitools::server::api;
 
@@ -6,6 +8,17 @@ int main() {
   const auto service = std::make_shared<restbed::Service>();
   const auto settings = std::make_shared<restbed::Settings>();
   settings->set_port(1234);
+  settings->set_root("/v2");
+
   auto petApi = PetApi(service, settings);
-  petApi.startService();
+  petApi.publishResources();
+
+  auto storeApi = StoreApi(service, settings);
+  storeApi.publishResources();
+
+  auto userApi = UserApi(service, settings);
+  userApi.publishResources();
+
+  service->start(settings);
+
 }

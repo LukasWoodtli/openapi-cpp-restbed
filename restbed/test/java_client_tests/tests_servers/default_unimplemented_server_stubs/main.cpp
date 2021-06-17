d1168eea8aaf4cf13d4d48e7f0f0b2ed670ef7a5
lukaswoodtli <woodtli.lukas@gmail.com>
2021-06-17 09:35:01 +0200

    Cleanup main of test

--- restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
+++ restbed/test/java_client_tests/tests_servers/default_unimplemented_server_stubs/main.cpp
@@ -6,8 +6,6 @@ using namespace org::openapitools::server::api;
 
 int main() {
   const auto service = std::make_shared<restbed::Service>();
-  const auto settings = std::make_shared<restbed::Settings>();
-  settings->set_port(1234);
 
   auto petApi = PetApi(service);
   petApi.publishDefaultResources();
@@ -17,6 +15,9 @@ int main() {
 
   auto userApi = UserApi(service);
   userApi.publishDefaultResources();
+
+  const auto settings = std::make_shared<restbed::Settings>();
+  settings->set_port(1234);
 
   service->start(settings);
 

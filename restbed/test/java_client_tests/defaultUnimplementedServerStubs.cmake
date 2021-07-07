b787cc9f314fc30ed4b683a682304d4c2750ee04
lukaswoodtli <woodtli.lukas@gmail.com>
2021-07-07 17:40:35 +0200

    Extract code that sends response into own function

--- restbed/test/java_client_tests/defaultUnimplementedServerStubs.cmake
+++ restbed/test/java_client_tests/defaultUnimplementedServerStubs.cmake
@@ -1,6 +1,13 @@
 
 add_custom_target(run_all_java_client_test_for_cpp_server)
 
+define_property(GLOBAL
+        PROPERTY LAST_JAVA_CLIENT_TEST
+        BRIEF_DOCS Used to order the tests to run after each other
+        FULL_DOCS The tests are ordered by defining a dependency chain)
+set_property(GLOBAL
+        PROPERTY LAST_JAVA_CLIENT_TEST run_all_java_client_test_for_cpp_server)
+
 set(RUN_CLIENT_TESTS_SHELL_TEMPLATE "${CMAKE_CURRENT_SOURCE_DIR}/run_java_client_tests_template.txt")
 
 function(run_java_client_test_for_cpp_server TARGET_NAME)
@@ -17,5 +24,12 @@ function(run_java_client_test_for_cpp_server TARGET_NAME)
             USES_TERMINAL
             COMMENT "Running tests: ${TARGET_NAME}")
 
+    get_property(LAST_TEST
+            GLOBAL
+            PROPERTY LAST_JAVA_CLIENT_TEST)
     add_dependencies(run_all_java_client_test_for_cpp_server ${RUN_TESTS_TARGET})
+    add_dependencies(${LAST_TEST} ${RUN_TESTS_TARGET})
+    set_property(GLOBAL
+            PROPERTY LAST_JAVA_CLIENT_TEST ${RUN_TESTS_TARGET})
+
 endfunction()
